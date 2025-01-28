ef9bb09f80312067fa53f06b8a3d8353e44617dd
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-28 01:52:31 +0000

    Calculate positions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{collections::HashMap, fs::File};
 
 use anyhow::Result;
 use chrono::{DateTime, NaiveDateTime, Utc};
@@ -6,14 +6,14 @@ use csv::ReaderBuilder;
 use serde::{de, Deserialize, Deserializer};
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct TxId(String);
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Deserialize)]
 struct RefId(String);
 
-#[derive(PartialEq, Debug, Deserialize)]
+#[derive(Clone, PartialEq, Debug, Deserialize)]
 #[serde(rename_all = "lowercase")]
 enum TransactionType {
     Deposit,
@@ -25,7 +25,7 @@ enum TransactionType {
     Other,
 }
 
-#[derive(PartialEq, Debug, Deserialize)]
+#[derive(Clone, PartialEq, Debug, Deserialize)]
 #[serde(rename_all = "UPPERCASE")]
 enum Asset {
     Usd,
@@ -38,7 +38,7 @@ enum Asset {
 }
 
 #[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct Transaction {
     txid: TxId,
     refid: RefId,
@@ -64,12 +64,50 @@ where
         .map_err(de::Error::custom)
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Position {
+    open: Transaction,
+    close: Option<Transaction>,
+    rollovers: Vec<Transaction>,
+}
+
 fn main() -> Result<()> {
     let file = File::open("input/ledgers.csv")?;
     let mut rdr = ReaderBuilder::new().has_headers(true).from_reader(file);
 
     let transactions: Vec<Transaction> = rdr.deserialize().map(|t| t.unwrap()).collect();
-    dbg!(transactions);
+
+    let mut positions: HashMap<RefId, Position> = HashMap::new();
+
+    for transaction in transactions {
+        match transaction.r#type {
+            TransactionType::Margin => {
+                if let Some(position) = positions.get_mut(&transaction.refid) {
+                    assert!(position.close.is_none());
+                    position.close = Some(transaction.clone());
+                } else {
+                    positions.insert(
+                        transaction.refid.clone(),
+                        Position {
+                            open: transaction.clone(),
+                            close: None,
+                            rollovers: Vec::new(),
+                        },
+                    );
+                }
+            }
+            TransactionType::Rollover => {
+                let position = positions.get_mut(&transaction.refid).unwrap();
+                position.rollovers.push(transaction.clone());
+            }
+            _ => {}
+        }
+    }
+
+    dbg!(positions);
+
+    // println!("{margins:#?}");
 
     Ok(())
 }
